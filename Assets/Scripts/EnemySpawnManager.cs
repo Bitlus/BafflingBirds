aec5ee23eb68eec7de031c3774a9fe8ae54e9fd9
James Olsen <jdwo@live.com>
2021-05-29 18:49:23 +1200

    created a spawner prefab

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
+    public float startDelay = 5.0f;
+    public float spawnInterval = 3.0f;
+    public GameObject[] obstacles;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomObject", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnRandomObject()
     {
-        
+        int index = Random.Range(0, obstacles.Length);
+        Instantiate(obstacles[index], transform);
     }
 }
