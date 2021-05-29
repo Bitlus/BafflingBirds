e1c25c252b72d7ece1230dcb9cd5fa5a77fc294f
James Olsen <jdwo@live.com>
2021-05-30 09:54:26 +1200

    made spawn rate random

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
-    public float startDelay = 5.0f;
-    public float spawnInterval = 3.0f;
+    public float minSpawnInterval = 2.0f;
+    public float maxSpawnInterval = 4.0f;
     public GameObject[] obstacles;
     
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomObject", startDelay, spawnInterval);
+        float spawnDelay = Random.Range(minSpawnInterval, maxSpawnInterval);
+        Invoke("SpawnRandomObject", spawnDelay);
     }
 
     private void SpawnRandomObject()
     {
         int index = Random.Range(0, obstacles.Length);
         Instantiate(obstacles[index], transform);
+        
+        float spawnDelay = Random.Range(minSpawnInterval, maxSpawnInterval);
+        Invoke("SpawnRandomObject", spawnDelay);
     }
 }
